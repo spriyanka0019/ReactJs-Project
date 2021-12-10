c96924e8472fb7fc0b1865c48f6d35fa909141d3
spriyanka0019 <spriyanka0019@gmail.com>
2021-12-10 11:48:09 +0530

    Added Login Button in the Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Fragment } from 'react';
 import logo from '../../assets/logo.svg';
 import './Header.css'
+import Button from "@material-ui/core/Button";
 
-export default function Header(){
+
+export default function Header(props){
     return(
         <div>
             <div  className="header" >
             <img  className="logo" src = {logo}  alt="Logo"/>
+            <Button className="buttonLogin" variant="contained">Login</Button>
             </div>
         </div>
 
